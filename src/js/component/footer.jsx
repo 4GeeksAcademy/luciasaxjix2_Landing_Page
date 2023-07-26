9b73d31ab71f51026998a0c8ecfe36be5a2a17bf
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-07-26 20:59:19 +0000

    Finalizado

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Footer = () => {
+
+    return (
+        <div className="container-fluid bg-dark mt-5">
+            <div className="container text-white p-4">
+                <div className="row">
+                    <div className="col-12">
+
+                        <p className ="text-center"> CopyRight -2023</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+export default Footer
